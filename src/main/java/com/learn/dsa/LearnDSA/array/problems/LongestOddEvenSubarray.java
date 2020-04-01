23e0ab6e330e04d76cc7108f182d3bbe1be7b5c5
sivasankarchinnasamy <siva.vnit.1986@gmail.com>
2020-04-01 22:22:50 +0530

    Array problems

---
+++ src/main/java/com/learn/dsa/LearnDSA/array/problems/LongestOddEvenSubarray.java
@@ -0,0 +1,68 @@
+package com.learn.dsa.LearnDSA.array.problems;
+
+/*
+ * Problem
+----------------------------------------------------------
+Given an array of size n. The problem is to find the length of the subsequence in the given array such that all the elements of the subsequence are sorted in increasing order and also they are alternately odd and even.
+Note that the subsequence could start either with the odd number or with the even number.
+
+Examples:
+
+Input : arr[] = {5, 6, 9, 4, 7, 8}
+Output : 4
+{5, 6, 7, 8} is the required longest increasing odd even subsequence.
+
+Input : arr[] = {1, 12, 2, 22, 5, 30, 31, 14, 17, 11}
+Output : 5
+
+ * Solution
+----------------------------------------------------------
+Efficient Approach:
+Let L(i) be the length of the LIOES (Longest Increasing Odd Even Subsequence) ending at index i such that arr[i] is the last element of the LIOES.
+Then, L(i) can be recursively written as:
+L(i) = 1 + max( L(j) ) where 0 < j < i and (arr[j] < arr[i]) and (arr[i]+arr[j])%2 != 0; or
+L(i) = 1, if no such j exists.
+To find the LIOES for a given array, we need to return max(L(i)) where 0 < i < n.
+A dynamic programming approach has been implemented below for the above mentioned recursive relation.
+
+ * */
+
+import java.util.*;
+import java.lang.*;
+
+public class LongestOddEvenSubarray {
+
+	// function to find the longest increasing odd even subsequence
+	public static int longOddEvenIncSeq(int arr[], int n) {
+		// lioes[i] stores longest increasing odd even subsequence ending at arr[i]
+		int[] lioes = new int[n];
+
+		// to store the length of longest increasing odd even subsequence
+		int maxLen = 0;
+
+		// Initialize LIOES values for all indexes
+		for (int i = 0; i < n; i++)
+			lioes[i] = 1;
+
+		// Compute optimized LIOES values in bottom up manner
+		for (int i = 1; i < n; i++)
+			for (int j = 0; j < i; j++)
+				if (arr[i] > arr[j] && (arr[i] + arr[j]) % 2 != 0 && lioes[i] < lioes[j] + 1)
+					lioes[i] = lioes[j] + 1;
+
+		// Pick maximum of all LIOES values
+		for (int i = 0; i < n; i++)
+			if (maxLen < lioes[i])
+				maxLen = lioes[i];
+
+		// required maximum length
+		return maxLen;
+	}
+
+	// driver function
+	public static void main(String argc[]) {
+		int[] arr = new int[] { 1, 12, 2, 22, 5, 30, 31, 14, 17, 11 };
+		int n = 10;
+		System.out.println("Longest Increasing Odd" + " Even Subsequence: " + longOddEvenIncSeq(arr, n));
+	}
+}
---
+++ src/main/java/com/learn/dsa/LearnDSA/array/problems/MaxSubarraySum.java
@@ -0,0 +1,82 @@
+package com.learn.dsa.LearnDSA.array.problems;
+
+import java.util.*;
+
+/*
+ * Problem
+--------------------------------------------------------------------------------------
+ You are given a one dimensional array that may contain both positive and negative integers, 
+ find the sum of contiguous subarray of numbers which has the largest sum. For example, if the 
+ given array is {-2, -5, 6, -2, -3, 1, 5, -6}, then the maximum subarray sum is 7.
+ 
+ * Solution: Divide & Conquer Algorithm
+--------------------------------------------------------------------------------------
+1) Divide the given array in two halves
+2) Return the maximum of following three
+….a) Maximum subarray sum in left half (Make a recursive call)
+….b) Maximum subarray sum in right half (Make a recursive call)
+….c) Maximum subarray sum such that the subarray crosses the midpoint
+
+The lines 2.a and 2.b are simple recursive calls. How to find maximum subarray sum such that the subarray crosses the midpoint? 
+We can easily find the crossing sum in linear time. The idea is simple, find the maximum sum starting from mid point and ending at 
+some point on left of mid, then find the maximum sum starting from mid + 1 and ending with sum point on right of mid + 1. 
+Finally, combine the two and return.
+
+Using Divide and Conquer approach, we can find the maximum subarray sum in O(nLogn) time
+ * */
+
+class MaxSubarraySum {
+
+	// Find the maximum possible sum in arr[] such that arr[m] is part of it
+	static int maxCrossingSum(int arr[], int l, int m, int h) {
+		// Include elements on left of mid.
+		int sum = 0;
+		int left_sum = Integer.MIN_VALUE;
+		for (int i = m; i >= l; i--) {
+			sum = sum + arr[i];
+			if (sum > left_sum)
+				left_sum = sum;
+		}
+
+		// Include elements on right of mid
+		sum = 0;
+		int right_sum = Integer.MIN_VALUE;
+		for (int i = m + 1; i <= h; i++) {
+			sum = sum + arr[i];
+			if (sum > right_sum)
+				right_sum = sum;
+		}
+
+		// Return sum of elements on left
+		// and right of mid
+		return left_sum + right_sum;
+	}
+
+	// Returns sum of maxium sum subarray
+	// in aa[l..h]
+	static int maxSubArraySum(int arr[], int l, int h) {
+		// Base Case: Only one element
+		if (l == h)
+			return arr[l];
+
+		// Find middle point
+		int m = (l + h) / 2;
+
+		/*
+		 * Return maximum of following three possible cases: a) Maximum subarray sum in
+		 * left half b) Maximum subarray sum in right half c) Maximum subarray sum such
+		 * that the subarray crosses the midpoint
+		 */
+		return Math.max(Math.max(maxSubArraySum(arr, l, m), maxSubArraySum(arr, m + 1, h)),
+				maxCrossingSum(arr, l, m, h));
+	}
+
+	/* Driver program to test maxSubArraySum */
+	public static void main(String[] args) {
+		int arr[] = { 2, 3, 4, 5, 7 };
+		int n = arr.length;
+		int max_sum = maxSubArraySum(arr, 0, n - 1);
+
+		System.out.println("Maximum contiguous sum is " + max_sum);
+	}
+}
