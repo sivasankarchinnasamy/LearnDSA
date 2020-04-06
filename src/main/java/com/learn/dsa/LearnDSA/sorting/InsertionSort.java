6bdb8dcdfea255ccd499967444c7230e163a4fda
sivasankarchinnasamy <siva.vnit.1986@gmail.com>
2020-04-06 21:32:55 +0530

    LinkedList Basics

--- src/main/java/com/learn/dsa/LearnDSA/sorting/InsertionSort.java
+++ src/main/java/com/learn/dsa/LearnDSA/sorting/InsertionSort.java
@@ -4,7 +4,8 @@ public class InsertionSort {
 
 	//static int[] arr = { 8, 7, 1, 42, 23, 55 };
 	//static int[] arr = 	{7, 10, 4, 3, 20, 15, 6};
-	static int[] arr = 	{7, 10, 4, 3, 20, 15, 6,45,56,23,78,23,89,99,1,2,4,4,5};
+	//static int[] arr = 	{7, 10, 4, 3, 20, 15, 6,45,56,23,78,23,89,99,1,2,4,4,5};
+	static int[] arr = 	{99,98,95,90,89,87,80,75,73,69,63,60,59,53,50,49,43,30,25,20,10,5,1};
 
 	public static void main(String[] args) {
 		InsertionSort sort = new InsertionSort();
